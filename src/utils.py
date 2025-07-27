d48ae35fc5577a564bb66bd61d1b17df6f40dfab
Abhay-k12 <abhaykanojia51@gmail.com>
2025-07-28 01:04:38 +0530

    Updation into core files

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-import jsonschema
+from jsonschema import validate
 
 OUTLINE_SCHEMA = {
     "type": "object",
@@ -21,4 +21,20 @@ OUTLINE_SCHEMA = {
 }
 
 def validate_json_schema(data):
-    jsonschema.validate(instance=data, schema=OUTLINE_SCHEMA)
+    # Ensure outline is always a list of proper objects
+    if not isinstance(data.get("outline"), list):
+        data["outline"] = []
+    
+    # Validate each outline item
+    valid_outline = []
+    for item in data["outline"]:
+        if isinstance(item, dict) and all(k in item for k in ["level", "text", "page"]):
+            valid_outline.append(item)
+    
+    data["outline"] = valid_outline or [{
+        "level": "H1",
+        "text": data.get("title", "Document Headings"),
+        "page": 1
+    }]
+    
+    validate(instance=data, schema=OUTLINE_SCHEMA)
